48fd18c919ea241897a726a23493a3a62a05e618
M.G.Swapna <124049045+swapnamgsk@users.noreply.github.com>
2023-04-13 12:55:32 +0530

    Add files via upload
    intersection

---
+++ week_01/day_7/intersection.java
@@ -0,0 +1,27 @@
+/*Write a program to find intersection of two sorted arrays
+Input: arr1[] = {1, 3, 4, 5, 7}
+        arr2[] = {2, 3, 5, 6}
+        Output: Intersection : {3, 5}
+*/ 
+
+
+class intersection
+{
+	public static void main(String[] args)
+	{
+		int array1[] = {1, 3, 4, 5, 7};
+        	int array2[] = {2, 3, 5, 6};
+        	
+        	for(int i =0;i<array1.length;i++)
+        	{
+        		for(int j = 0;j<array2.length;j++)
+        		{
+        			if(array1[i] == array2[j])
+        			{
+        				System.out.print(array1[i] + " ");
+        			}
+        		}
+        	}
+
+	}
+}
