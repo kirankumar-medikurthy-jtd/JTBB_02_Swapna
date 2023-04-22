deca846e8c91f43c75e634400de4c0d0e5ce25f0
swapnamgsk <mgswapna1911@gmail.com>
2023-04-22 13:36:51 +0530

    front end assignment

---
+++ week_01/day_1/FE/practiframe.java
@@ -0,0 +1,31 @@
+<html>
+   <head>
+      <title>Logos</title>
+   </head>
+   <body>
+     <div>
+     	<h1>Group-4</h1>
+     	<h1 style="text-align:right">
+     		<a href="">Back To Normal</a>
+     	</h1>
+        <ol>
+           <li><a href="https://www.booking.com/" target="myiframe"><img width="90px" src="makeup.jpg"></a></li>
+           <li>
+           	<a href="https://workspace.google.com/" target="myiframe">
+           		<img width="90px" src="Linkedinlogo.jpeg"/>
+           	</a>
+           </li>
+           <li>
+           	<a href="https://chocolatephilosophy.co.in/" target="myiframe">
+           		<img width="90px" src="gitlogo.png"/>
+           	</a>
+           </li>
+        </ol>
+     </div>
+     
+     <iframe height="600px" width="100%" style="border:5px solid red;" name="myiframe" title="frame" src="ABN.html">
+       
+     </iframe>
+ 
+   </body>
+</html>
---
+++ week_01/day_1/FE/table2.java
@@ -0,0 +1,49 @@
+<html>
+<head>
+<title>Assaignment</title>
+</head>
+<body>
+	<table border = "1" cellspacing = "0" width = "400px" height = "200px">
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+			<td>6</td>
+			<td>7</td>
+			<td>8</td>
+			<td>9</td>
+			<td rowspan = "3">10</td>
+		
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+			<td>6</td>
+			<td>7</td>
+			<td>8</td>
+			<td>9</td>
+			<td rowspan = "3">10</td>
+		
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td>4</td>
+			<td>5</td>
+			<td>6</td>
+			<td>7</td>
+			<td>8</td>
+			<td>9</td>
+			<td rowspan = "3">10</td>
+		
+		</tr>
+		
+	</table>
+</body>	
+</html>	
